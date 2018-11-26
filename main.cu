37f546d4d62089c85482bbcb9bac0aca904b19ad
mutsuki <mutsuki@momo86.net>
2018-11-26 22:45:20 +0900

    update : specify kernel function by template argument

--- main.cu
+++ main.cu
@@ -17,18 +17,18 @@ __device__ T sign(const T v){
 }
 
 template <class T>
-__global__ void kernel_if(T* a, T* b){
+__global__ void kernel_if(const T* const a, T* const b){
 	const auto tid = threadIdx.x + blockIdx.x * blockDim.x;
 	b[tid] = sign(a[tid]);
 }
 template <class T>
-__global__ void kernel_cutf(T* a, T* b){
+__global__ void kernel_cutf(const T* const a, T* const b){
 	const auto tid = threadIdx.x + blockIdx.x * blockDim.x;
 	b[tid] = cutf::cuda::math::sign(a[tid]);
 }
 
-template <std::size_t N, std::size_t C, class T>
-void test_signed_0(){
+template <std::size_t N, std::size_t C, class T, class Func>
+void test(Func func){
 	std::cout<<__func__<<std::endl;
 	auto dF = cutf::cuda::memory::get_device_unique_ptr<T>(N);
 	auto hF = cutf::cuda::memory::get_host_unique_ptr<T>(N);
@@ -41,37 +41,29 @@ void test_signed_0(){
 	cutf::cuda::memory::copy(dF.get(), hF.get(), N);
 
 	for(std::size_t c = 0; c < C; c++)
-		kernel_if<T><<<N, 1>>>(dF.get(), dI.get());
+		func(dF.get(), dI.get());
 
 	cutf::cuda::memory::copy(hI.get(), dI.get(), N);
 }
-template <std::size_t N, std::size_t C, class T>
-void test_signed_1(){
-	std::cout<<__func__<<std::endl;
-	auto dF = cutf::cuda::memory::get_device_unique_ptr<T>(N);
-	auto hF = cutf::cuda::memory::get_host_unique_ptr<T>(N);
-	auto dI = cutf::cuda::memory::get_device_unique_ptr<T>(N);
-	auto hI = cutf::cuda::memory::get_host_unique_ptr<T>(N);
-	for(auto i = decltype(N)(0); i < N; i++){
-		hF.get()[i] = cutf::cuda::type::cast<T>((static_cast<float>(N/2) - i) * 10.0f);
-	}
 
-	cutf::cuda::memory::copy(dF.get(), hF.get(), N);
-
-	for(std::size_t c = 0; c < C; c++)
-		kernel_cutf<T><<<N, 1>>>(dF.get(), dI.get());
-
-	cutf::cuda::memory::copy(hI.get(), dI.get(), N);
+template <std::size_t N, class T>
+void test_if(const T* const a, T* const b){
+	kernel_if<T><<<N, 1>>>(a, b);
 }
+template <std::size_t N, class T>
+void test_cutf(const T* const a, T* const b){
+	kernel_cutf<T><<<N, 1>>>(a, b);
+}
+
 
 int main(){
 	constexpr std::size_t N = 1 << 10;
 	constexpr std::size_t C = 1 << 13;
 
-	test_signed_0<N, C, half>();
-	test_signed_1<N, C, half>();
-	test_signed_0<N, C, float>();
-	test_signed_1<N, C, float>();
-	test_signed_0<N, C, double>();
-	test_signed_1<N, C, double>();
+	test<N, C, half>(test_if<N, half>);
+	test<N, C, float>(test_if<N, float>);
+	test<N, C, double>(test_if<N, double>);
+	test<N, C, half>(test_cutf<N, half>);
+	test<N, C, float>(test_cutf<N, float>);
+	test<N, C, double>(test_cutf<N, double>);
 }
